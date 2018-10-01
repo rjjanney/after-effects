6b1249b0307702139191b08a6561d952793246dd
Randy Janney <rjjanney@gmail.com>
2018-10-01 13:57:29 -0400

    Add file / project operations.

--- hello.js
+++ hello.js
@@ -2,6 +2,14 @@ var http = require('http');
 var dt = require('./myfirstmodule');
 var ae = require('../after-effects');
 
+var initializeProject = ae(() => {
+    if (app.project) {
+        app.project.close(CloseOptions.DO_NOT_SAVE_CHANGES);
+    }
+    var my_file = new File("/Users/206098175/Projects/after-effects/test_project.aep");
+    app.open(my_file);
+});
+
 // THIS IS A FUNCTION DEFINITION, but the value is only available to node.js (?)
 var myposition = ae(() => {
     return app.project.item(1).layer(1).property("Position").value;
@@ -14,52 +22,52 @@ var foo = () => app.project.item(1).layer(1).duplicate();
 ae(foo); // SENDS AND RUNS FUNCTION foo IN AE
 
 // THIS IS A FUNCTION DEFINITION, MULTIPLE LINES, AND IT WORKS!
-var settext = () => {app.project.item(1).layer(1).duplicate();
-const hipp = app.project.item(1).layer(1);
-const hopp = app.project.item(1).layer(2);
+var settext = () => {
+    app.beginUndoGroup("nodeScript");
+    app.project.item(1).layer(1).duplicate();
+    const hipp = app.project.item(1).layer(1);
+    const hopp = app.project.item(1).layer(2);
 
 
 
-// Here is a keyframing the source text property (although it's different from the Scripting guide)
-var text1Prop = hopp.property("Source Text");
-text1Prop.setValueAtTime(0, new TextDocument("Happy"));
-text1Prop.setValueAtTime(.5, new TextDocument("cake"));
-text1Prop.setValueAtTime(1, new TextDocument("is"));
-text1Prop.setValueAtTime(1.5, new TextDocument("yummy!"));
+    // Here is a keyframing the source text property (although it's different from the Scripting guide)
+    var text1Prop = hopp.property("Source Text");
+    text1Prop.setValueAtTime(0, new TextDocument("Happy"));
+    text1Prop.setValueAtTime(.5, new TextDocument("cake"));
+    text1Prop.setValueAtTime(1, new TextDocument("is"));
+    text1Prop.setValueAtTime(1.5, new TextDocument("yummy!"));
 
 
 
-// Here is a whole text manipulation thing from the AE Scripting Guide "Text Document Object" section
-var textProp = hipp.property("Source Text");
-var textDocument = textProp.value;
-myString = "Happy holidays!";
-textDocument.resetCharStyle();
-textDocument.fontSize = 60;
-textDocument.fillColor = [1, 0, 0];
-textDocument.strokeColor = [0, 1, 0];
-textDocument.strokeWidth = 2;
-textDocument.font = "GCFrankBold";
-textDocument.strokeOverFill = true;
-textDocument.applyStroke = true;
-textDocument.applyFill = true;
-textDocument.text = myString.toUpperCase();
-textDocument.justification = ParagraphJustification.CENTER_JUSTIFY;
-textDocument.tracking = 50;
-textProp.setValue(textDocument);
+    // Here is a whole text manipulation thing from the AE Scripting Guide "Text Document Object" section
+    var textProp = hipp.property("Source Text");
+    var textDocument = textProp.value;
+    myString = "Happy holidays!";
+    textDocument.resetCharStyle();
+    textDocument.fontSize = 60;
+    textDocument.fillColor = [1, 0, 0];
+    textDocument.strokeColor = [0, 1, 0];
+    textDocument.strokeWidth = 2;
+    textDocument.font = "GCFrankBold";
+    textDocument.strokeOverFill = true;
+    textDocument.applyStroke = true;
+    textDocument.applyFill = true;
+    textDocument.text = myString.toUpperCase();
+    textDocument.justification = ParagraphJustification.CENTER_JUSTIFY;
+    textDocument.tracking = 50;
+    textProp.setValue(textDocument);
 
 
-var myposition = app.project.item(1).layer(1).property("Position").value;
-app.project.item(1).layer(1).property("Position").setValue([myposition[0], myposition[1] + 200]);
-app.project.item(1).layer("sdfaf").property("Transform").property("Position").setValue([myposition[0], myposition[1] + 100]); // "Transform" is optional
+    var myposition = app.project.item(1).layer(1).property("Position").value;
+    app.project.item(1).layer(1).property("Position").setValue([myposition[0], myposition[1] + 200]);
+    app.project.item(1).layer("sdfaf").property("Transform").property("Position").setValue([myposition[0], myposition[1] + 100]); // "Transform" is optional
+    app.endUndoGroup();
 };
 
 ae(settext); // SENDS AND RUNS FUNCTION settext IN AE
 
 // RUNS COMMAND IN DIRECTLY IN AE 
 ae(() => app.project.item(1).layer(3).property("Source Text").setValue("nonsense words here"));
-
-
-// console.log(myposition[0]);
 
 // ae((myposition)=>   app.project.item(1).layer(1).property("Transform").property("Position").setValue([myposition[0], myposition[1]+300]));
 
