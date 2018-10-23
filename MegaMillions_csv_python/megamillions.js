e1087890f284d5568d8e7ed35417e62902786dc3
Randy Janney <rjjanney@gmail.com>
2018-10-23 18:54:59 -0400

    JavaScript version sort of working.

--- MegaMillions_csv_python/megamillions.js
+++ MegaMillions_csv_python/megamillions.js
@@ -1,38 +1,36 @@
-/* var fs = require('fs');
+var csv = require('csv'); 
+// loads the csv module referenced above.
 
-fs.readFile('megamillions_raw.txt', function (err, data) {
-    if (data) {
-       
-        for (line in data) {
+var obj = csv(); 
+// gets the csv module to access the required functionality
 
-        console.log(data[line]);
+function MyCSV(Fone, Ftwo, Fthree) {
+    this.FieldOne = Fone;
+    this.FieldTwo = Ftwo;
+    this.FieldThree = Fthree;
+}; 
+// Define the MyCSV object with parameterized constructor, this will be used for storing the data read from the csv into an array of MyCSV. You will need to define each field as shown above.
+
+var MyData = []; 
+// MyData array will contain the data from the CSV file and it will be sent to the clients request over HTTP. 
+
+obj.from.path('megamillions.csv').to.array(function (data) {
+    for (var index = 0; index < data.length; index++) {
+        MyData.push(new MyCSV(data[index][0], data[index][1], data[index][2]));
     }
-   
-    }
-})  
-let holder = [];
-var lineReader = require('readline').createInterface({
-  input: require('fs').createReadStream('megamillions_raw.txt')
+    console.log(MyData);
 });
+//Reads the CSV file from the path you specify, and the data is stored in the array we specified using callback function.  This function iterates through an array and each line from the CSV file will be pushed as a record to another array called MyData , and logs the data into the console to ensure it worked.
 
-lineReader.on('line', function (line) {
-    holder.push(line);
+var http = require('http');
+//Load the http module.
+
+var server = http.createServer(function (req, resp) {
+    resp.writeHead(200, { 'content-type': 'application/json' });
+    resp.end(JSON.stringify(MyData));
 });
+// Create a webserver with a request listener callback.  This will write the response header with the content type as json, and end the response by sending the MyData array in JSON format.
 
-console.log(holder);
-*/
+server.listen(1080);
+// Tells the webserver to listen on port 8080(obviously this may be whatever port you want.)
 
-var lineReader = require('line-reader');
-
-lineReader.eachLine(('megamillions_raw.txt', holder=[]), function(line, last) {
-  if (line) {
-  holder.push(line);
-  }
-  // do whatever you want with line...
-  if(last){
-    // or check if it's the last one
-    // console.log(holder);
-  }
-  return holder;
-});
-console.log(lineReader.number);
